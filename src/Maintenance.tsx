48bd4c8d463e95f05d1725406193d71e9548eb98
SotaFukumoto <key9ba9gt9works@gmail.com>
2025-03-21 03:38:02 +0900

    created Maintenance page

---
+++ src/Maintenance.tsx
@@ -0,0 +1,19 @@
+import './Maintenance.css'
+
+function Maintenance() {
+    return (
+      <div>
+        <div className="animation flex justify-center">
+            <div className="one spin-one"></div>
+            <div className="two spin-two"></div>
+            <div className="three spin-one"></div>
+        </div>
+        <div>
+          <p>現在メンテナンス中</p>
+        </div>
+        <p className="footer">© 2025 SotaFukumoto All Rights Reserved.</p>
+      </div>
+    );
+  };
+
+export default Maintenance
