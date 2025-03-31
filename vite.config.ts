25eadf616e57cc9930caef2a118317fff9985d05
SotaFukumoto <key9ba9gt9works@gmail.com>
2025-03-31 22:49:42 +0900

    pre publishing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import license from "rollup-plugin-license"
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    license({
+      thirdParty: {
+        includePrivate: true, // Default is false.
+        includeSelf: true, // Default is false.
+        multipleVersions: true, // Default is false.
+        output: path.join(__dirname, 'dist', 'licenses.txt'),
+      }
+    })
+  ],
+  server: {
+    watch: {
+      usePolling: true
+    }
+  },
 })
