a5e8ef5791bace2fde64d96a9c8998946f7a2df9
SotaFukumoto <key9ba9gt9works@gmail.com>
2025-05-12 09:30:59 +0900

    add react-bits

--- src/Sandbox.tsx
+++ src/Sandbox.tsx
@@ -1,5 +1,4 @@
 import './Sandbox.css'
-import { Button } from "@/components/ui/button"
 import {
   Drawer,
   DrawerClose,
@@ -10,30 +9,40 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer"
+import { Button } from "@/components/ui/button"
+import Dock from "./reactbits/Dock/Dock.tsx"
+import { VscHome,VscArchive,VscAccount,VscSettingsGear } from "react-icons/vsc";
 
 function Sandbox() {
+  const items = [
+    { icon: <VscHome size={18} />, label: 'Home', onClick: () => alert('Home!') },
+    { icon: <VscArchive size={18} />, label: 'Archive', onClick: () => alert('Archive!') },
+    { icon: <VscAccount size={18} />, label: 'Profile', onClick: () => alert('Profile!') },
+    { icon: <VscSettingsGear size={18} />, label: 'Settings', onClick: () => alert('Settings!') },
+  ];
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-      <Drawer>
-        <DrawerTrigger>
-          Open
-        </DrawerTrigger>
-        <DrawerContent>
-          <DrawerHeader>
-            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-            <DrawerDescription>This action cannot be undone.</DrawerDescription>
-          </DrawerHeader>
-          <DrawerFooter>
-            <Button>Submit</Button>
-            <DrawerClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
-    </div>
+    <Drawer>
+      <DrawerTrigger>Open<VscHome size={18} /></DrawerTrigger>
+      <DrawerContent>
+        <Dock 
+          items={items}
+          panelHeight={68}
+          baseItemSize={50}
+          magnification={70}
+        />
+        {/* <DrawerHeader>
+          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+          <DrawerDescription>This action cannot be undone.</DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          <Button>Submit</Button>
+          <DrawerClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter> */}
+      </DrawerContent>
+    </Drawer>
   )
-};
+}
 
 export default Sandbox
